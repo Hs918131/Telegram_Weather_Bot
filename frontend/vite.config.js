d9554ea1747203070c3abe91aab62e2089e2e231
Harshit Sharma <91822610+Hs918131@users.noreply.github.com>
2025-02-15 23:08:48 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'dist', // This is the correct build output
+  },
   plugins: [react()],
 })
